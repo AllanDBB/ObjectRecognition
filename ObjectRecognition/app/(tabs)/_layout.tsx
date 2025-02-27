4eab836d7da10bacb504d29ea7fc78051913b2d4
Allan! <110310533+AllanDBB@users.noreply.github.com>
2025-02-26 21:58:18 -0600

    temp

--- ObjectRecognition/app/(tabs)/_layout.tsx
+++ ObjectRecognition/app/(tabs)/_layout.tsx
@@ -20,7 +20,6 @@ export default function TabLayout() {
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
@@ -34,10 +33,10 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="camera"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Camera',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="camera" color={color} />,
         }}
       />
     </Tabs>
